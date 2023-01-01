c0dceac359cb5fa1ac196e786c604fefac9631c1
Talles Cocchi <tallescocchi@gmail.com>
2022-12-31 23:40:17 -0300

    :tada: Commit inicial

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle('light')
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Foto de Talles Cocchi, sorrindo, com oculos escuro, usando camisa cinza e de cabelos longos soltos.')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('alt', 'Foto de Talles Cocchi, sorrindo, usando camisa cinza e de cabelos longos soltos.')
+  }
+}
